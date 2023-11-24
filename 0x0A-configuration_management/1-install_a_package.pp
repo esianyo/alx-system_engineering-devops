a08a1a653a25f90dba3239e160a57bad763a9175
esianyo <esianicd@gmail.com>
2023-11-24 23:32:44 +0000

    my partner was taken aback by my weird commit message

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,10 +1,10 @@
 # Installs puppet-lint package
 package { 'flask':
-  ensure =>  '2.1.0',
-  name   =>  'flask',
+  ensure   =>  '2.1.0',
+  provider =>  'pip3',
 }
 
 package { 'werkzeug':
-  ensure =>  '2.1.1',
-  name   =>  'werkzeug'
+  ensure   =>  '2.1.1',
+  provider =>  'pip3'
 }
