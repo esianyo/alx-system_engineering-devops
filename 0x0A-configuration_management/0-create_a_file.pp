9e6803d61243dd73f7b87c4fb0ffdb12ceed87ca
esianyo <esianicd@gmail.com>
2023-11-24 23:12:12 +0000

    my partner was taken aback by my weird commit message

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # Using Puppet to create a file in /tmp.
 file { '/tmp/school':
-  path  =>  '/tmp/school',
-  content  =>  'I love Puppet',
-  mode  =>  '0744',
-  owner =>  'www-data',
-  group =>  'www-data',
+  path    =>  '/tmp/school',
+  content =>  'I love Puppet',
+  mode    =>  '0744',
+  owner   =>  'www-data',
+  group   =>  'www-data',
 }
