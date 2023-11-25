fff36a22aeae1d09d3d09d2abdde3794a701cd76
esianyo <esianicd@gmail.com>
2023-11-25 18:52:52 +0000

    back to bash scripting

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
 # Escaping passwd request using Puppet
 
-node 'ubuntu' {
-  file_line { 'Turn off passwd auth':
-    ensure => absent,
-    path => '/etc/ssh/ssh_config',
-    match => '.*PasswordAuthentication .*',
-  }
+#!/usr/bin/env bash
+# using Puppet to make changes to our configuration file
 
-  file_line { 'Declare identity file':
-    ensure => present,
-    path => '/etc/ssh/ssh_config',
-    line => 'IdentityFile ~/.ssh/school',
-  }
+file { '/etc/ssh/ssh_config':
+  ensure  => present,
+  content => "
+    #SSH client configuration
+    host
+        HostName 54.197.45.145
+        User ubuntu
+        IdentityFile ~/.ssh/school
+        PasswordAuthentication no
+   "
 }
