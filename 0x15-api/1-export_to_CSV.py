691d20b37aab0d9927ca197f5bdab2856221d21a
esianyo <esianicd@gmail.com>
2024-01-02 23:55:11 +0000

    这是第4

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -3,42 +3,24 @@
 '''
 Script that exports an employee TODO tasks
 '''
+import csv
 import requests
 import sys
 
 
-def tasks_done(id):
-    '''Script that exports an employee TODO tasks to a csv file
-        Parameters:
-        employee_id: Is an interger representing an employee id.
-    '''
-
-    url = "https://jsonplaceholder.typicode.com/users/{}".format(id)
-    response = requests.get(url)
-    response_json = response.json()
-    employee_name = response_json["name"]
-
-    url = "https://jsonplaceholder.typicode.com/users/{}/todos".format(id)
-    todos = requests.get(url)
-    todos_json = todos.json()
-    number_tasks = len(todos_json)
-
-    task_compleated = 0
-    task_list = ""
-
-    file_name = "{}.csv".format(id)
-
-    with open(file_name, "a") as fd:
-        for todo in todos_json:
-            completed = todo.get("completed")
-            title = todo.get("title")
-            csv_data = "\"{}\",\"{}\",\"{}\",\"{}\"\n".format(id,
-                                                              employee_name,
-                                                              completed,
-                                                              title
-                                                              )
-            fd.write(csv_data)
-
-
 if __name__ == "__main__":
-    tasks_done(sys.argv[1])
+    if len(sys.argv) == 2:
+        req = "https://jsonplaceholder.typicode.com/users/{}/todos"\
+              .format(sys.argv[1])
+        response = requests.get(req).json()
+        req_use = "https://jsonplaceholder.typicode.com/users/{}"\
+                  .format(sys.argv[1])
+        info_user = requests.get(req_use).json()
+        name_user = info_user.get('username')
+        with open('{}.csv'.format(sys.argv[1]), 'w') as f:
+            writer = csv.writer(f, quoting=csv.QUOTE_ALL)
+            for content in response:
+                id_us = content['userId']
+                complet = content['completed']
+                content = content['title']
+                writer.writerow([id_us, name_user, complet, content])
