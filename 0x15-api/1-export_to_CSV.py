b74d2f7142cdd9a4ec671fb0df14dbc371166292
esianyo <esianicd@gmail.com>
2024-01-02 23:34:41 +0000

    这是第三

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,83 +1,40 @@
-#!/usr/bin/env python3
-"""
-Python script to fetch and export TODO list progress for a given employee ID.
-
-Usage:
-    ./export_to_CSV.py <employee_id>
-"""
-
+#!/usr/bin/python3
 import requests
-import csv
-from sys import argv
+import sys
 
 
-def fetch_employee_data(employee_id):
-    """
-    Fetch user and TODOs data for a given employee ID.
+def tasks_done(id):
+    '''Script that exports an employee TODO tasks to a csv file
+        Parameters:
+        employee_id: Is an interger representing an employee id.
+    '''
 
-    Args:
-        employee_id (int): Employee ID to fetch data for.
+    url = "https://jsonplaceholder.typicode.com/users/{}".format(id)
+    response = requests.get(url)
+    response_json = response.json()
+    employee_name = response_json["name"]
 
-    Returns:
-        Tuple: A tuple containing user data and a list of completed tasks.
-    """
-    user_url = 'https://jsonplaceholder.typicode.com/users/{}'.format(employee_id)
-    user_response = requests.get(user_url)
+    url = "https://jsonplaceholder.typicode.com/users/{}/todos".format(id)
+    todos = requests.get(url)
+    todos_json = todos.json()
+    number_tasks = len(todos_json)
 
-    todos_url = 'https://jsonplaceholder.typicode.com/todos?userId={}'.format(employee_id)
-    todos_response = requests.get(todos_url)
+    task_compleated = 0
+    task_list = ""
 
-    if user_response.status_code != 200:
-        print("User not found")
-        exit(1)
+    file_name = "{}.csv".format(id)
 
-    if todos_response.status_code != 200:
-        print("Todos not found")
-        exit(1)
-
-    user = user_response.json()
-    todos = todos_response.json()
-
-    completed_tasks = [task for task in todos if task['completed']]
-
-    return user, completed_tasks
-
-
-def export_to_csv(user, completed_tasks, csv_filename):
-    """
-    Export user's completed tasks to a CSV file.
-
-    Args:
-        user (dict): User data.
-        completed_tasks (list): List of completed tasks.
-        csv_filename (str): CSV file name to export data to.
-    """
-    with open(csv_filename, mode='w', newline='') as csv_file:
-        fieldnames = ['USER_ID', 'USERNAME', 'TASK_COMPLETED_STATUS', 'TASK_TITLE']
-        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-
-        writer.writeheader()
-
-        for task in completed_tasks:
-            writer.writerow({
-                'USER_ID': user['id'],
-                'USERNAME': user['username'],
-                'TASK_COMPLETED_STATUS': task['completed'],
-                'TASK_TITLE': task['title']
-            })
-
-    print("Data exported to {}".format(csv_filename))
+    with open(file_name, "a") as fd:
+        for todo in todos_json:
+            completed = todo.get("completed")
+            title = todo.get("title")
+            csv_data = "\"{}\",\"{}\",\"{}\",\"{}\"\n".format(id,
+                                                              employee_name,
+                                                              completed,
+                                                              title
+                                                              )
+            fd.write(csv_data)
 
 
 if __name__ == "__main__":
-    if len(argv) != 2 or not argv[1].isdigit():
-        print("Usage: {} <employee_id>".format(argv[0]))
-        exit(1)
-
-    employee_id = int(argv[1])
-
-    user, completed_tasks = fetch_employee_data(employee_id)
-
-    csv_filename = '{}.csv'.format(user['id'])
-
-    export_to_csv(user, completed_tasks, csv_filename)
+    tasks_done(sys.argv[1])
